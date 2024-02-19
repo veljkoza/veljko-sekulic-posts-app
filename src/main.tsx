9fbd01fe8b7daaa03cdef1e4c416bd6d8ffcb3d7
veljkoza <veljko.sekulic1998@gmail.com>
2024-02-19 22:39:21 +0100

    feat: sets up react router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,35 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/posts",
+    element: (
+      <div>
+        <h1>Posts</h1>
+      </div>
+    ),
+  },
+  {
+    path: "/posts/:id",
+    element: (
+      <div>
+        <h1>Posts Details</h1>
+      </div>
+    ),
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
-)
+);
