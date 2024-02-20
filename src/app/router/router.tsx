21e6920bcfaeae05cbd142b51818bdbdb1c7e004
veljkoza <veljko.sekulic1998@gmail.com>
2024-02-21 00:16:02 +0100

    fix: moves router files to app folder

--- src/app/router/router.tsx
+++ src/app/router/router.tsx
@@ -1,5 +1,5 @@
+import App from "@/App";
 import { createBrowserRouter } from "react-router-dom";
-import App from "../App";
 import { routes } from "./routes";
 
 export const router = createBrowserRouter([
