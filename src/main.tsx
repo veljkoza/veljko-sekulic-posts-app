dd2c1064e1e467b13eb5bdfce796d014083d4df7
veljkoza <veljko.sekulic1998@gmail.com>
2024-02-21 22:03:57 +0100

    fix: wraps app with HttpClientProvider

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,14 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 
 import { RouterProvider } from "react-router-dom";
-import { router } from "@/router";
+import { HttpClientProvider } from "./app/providers";
+import { router } from "./app/router";
+import { httpClient } from "./infrastructure";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <HttpClientProvider httpClient={httpClient}>
+      <RouterProvider router={router} />
+    </HttpClientProvider>
   </React.StrictMode>,
 );
