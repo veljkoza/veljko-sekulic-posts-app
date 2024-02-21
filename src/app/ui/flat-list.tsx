64e24ee416f9f71e3f496864666ffea102f96b03
veljkoza <veljko.sekulic1998@gmail.com>
2024-02-21 20:40:45 +0100

    feat: creates flat-list component

---
+++ src/app/ui/flat-list.tsx
@@ -0,0 +1,22 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+export type FlatListProps<T extends any[]> = {
+  data: T;
+  renderItem: (item: T[number]) => React.JSX.Element;
+  renderSeparator?: (item: T[number]) => React.JSX.Element;
+};
+
+export const FlatList = <T extends any[]>({
+  data,
+  renderItem,
+  renderSeparator,
+}: FlatListProps<T>) => {
+  return (
+    <div>
+      {data.map((item, index) => (
+        <>
+          {renderItem(item)} {index % 1 === 0 && renderSeparator?.(item)}
+        </>
+      ))}
+    </div>
+  );
+};
