b8e3fca1e247d3353d52160c1a95278224e89082
veljkoza <veljko.sekulic1998@gmail.com>
2024-02-21 21:58:10 +0100

    feat: creates button component

---
+++ src/app/ui/button/button.tsx
@@ -0,0 +1,35 @@
+import type { ComponentProps } from "react";
+import styles from "./button.module.css";
+const SIZE = {
+  mini: styles["button--mini"],
+  default: styles["button--default"],
+};
+const VARIANT = {
+  plain: styles["button--plain"],
+  primary: styles["button--primary"],
+};
+
+export const Button = ({
+  className = "",
+  size = "default",
+  variant = "primary",
+  href,
+  ...props
+}: {
+  size?: keyof typeof SIZE;
+  variant?: keyof typeof VARIANT;
+  href?: string;
+} & ComponentProps<"button">) => {
+  const computedClassName = `${styles.button} ${SIZE[size]} ${VARIANT[variant]} ${className}`;
+  if (href)
+    return (
+      <a href={href} className={computedClassName}>
+        {props.children}
+      </a>
+    );
+  return (
+    <button {...props} className={computedClassName}>
+      {props.children}
+    </button>
+  );
+};
