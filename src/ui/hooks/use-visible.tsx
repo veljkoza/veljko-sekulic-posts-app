e089a03bbe6d26d9289cdecebeca5401099718b5
veljkoza <veljko.sekulic1998@gmail.com>
2024-02-22 20:18:35 +0100

    update: expands useVisible hook

--- src/ui/hooks/use-visible.tsx
+++ src/ui/hooks/use-visible.tsx
@@ -3,10 +3,13 @@ import { MutableRefObject, useEffect, useState } from "react";
 export const useVisible = (
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   ref: MutableRefObject<any>,
-  options?: { onIntersect?: (entry: IntersectionObserverEntry) => void },
+  options?: {
+    onIntersect?: (entry: IntersectionObserverEntry) => void;
+    rootMargin?: string;
+  },
 ) => {
   const [isVisible, setIsVisible] = useState(false);
-  const { onIntersect } = options || {};
+  const { onIntersect, rootMargin = "100px" } = options || {};
   useEffect(() => {
     const observer = new IntersectionObserver(
       (entries) => {
@@ -18,7 +21,7 @@ export const useVisible = (
       },
       {
         root: null,
-        rootMargin: "0px",
+        rootMargin,
         threshold: 1.0,
       },
     );
